0ad3afaf543eefe573709274ddd4e37f9be23b53
infomagnum159 <infomagnum159@gmail.com>
2022-08-01 18:07:36 +0600

    Added a component for personal data

--- src/Components/OrderInfo/OrderInfo.js
+++ src/Components/OrderInfo/OrderInfo.js
@@ -7,16 +7,13 @@ const OrderInfo = props => {
     const onClickCLose = () => {
         props.onOrder();
     }
-
     return (
         <>
             <h3>Ваш Заказ</h3>
             <p>Введите Ваши данные</p>
-
             <ContactData
                 onClose={onClickCLose}
             />
-
             <p><strong>Сумма заказа: {props.price}  KGS</strong></p>
             <Button type="Danger" onClick={props.onCancel}>Отмена</Button>
         </>
